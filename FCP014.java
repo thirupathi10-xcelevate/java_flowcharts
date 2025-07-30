dc3be78b5aa6d8e052072acf7305afa913856491
thirupathi10-xcelevate <thirupathi.2537@xcelevate.org>
2025-07-30 10:27:06 +0530

    Add files via upload

---
+++ FCP014.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class FCP014{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+int b = sc.nextInt();
+int c = sc.nextInt();
+if (a > b){
+if (a > c){
+System.out.println(a);
+}
+}
+else if(b > c){
+System.out.println(b);
+}
+else{
+System.out.println(c);
+}
+}
+}
