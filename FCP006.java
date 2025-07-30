6dec05233abe23bb03b85523772b9cbcfa6408c6
thirupathi10-xcelevate <thirupathi.2537@xcelevate.org>
2025-07-30 09:52:41 +0530

    Add files via upload

---
+++ FCP006.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class FCP006 {
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int l = n % 10;
+    System.out.println(l);
+   }
+}
